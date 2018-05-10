b0b424a14af201b7b59695b7b0cecb51f29342d9
SanchitL <sanchit.luthra@mail.utoronto.ca>
2018-05-09 23:34:44 -0400

    Added description of network

--- mnist.py
+++ mnist.py
@@ -12,3 +12,15 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 if __name__ == "__main__":
     tf.app.run()
+
+
+'''
+1. Convolutional layer #1: 32 5x5 filters, with ReLU activation
+2. Pooling Layer #1: Max pooling with a 2x2 filter and stride of 2
+
+3. Convolutional Layer #2: 64 5x5 filters, with ReLU activation
+4. Polling Layer #2: max pooling with 2x2 filter and stride of 2
+
+5. Dense layer #1: 1,024 neurons, with dropout regularization rate of 0.4
+6. Dense Layer #2: 10 neurons, one for each digit target class (0-9).
+'''
